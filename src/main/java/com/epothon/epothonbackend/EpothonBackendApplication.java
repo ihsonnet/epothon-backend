61fcc93502d3715bb5b2131865f6565648a2e44e
Injamamul Haque Sonet <ihsonnet@gmail.com>
2022-03-28 03:18:48 +0600

    base spring boot

---
+++ src/main/java/com/epothon/epothonbackend/EpothonBackendApplication.java
@@ -0,0 +1,13 @@
+package com.epothon.epothonbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpothonBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpothonBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epothon/epothonbackend/EpothonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epothon.epothonbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpothonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
